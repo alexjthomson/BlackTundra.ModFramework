c3c5c41dcee9d8690ee644dcdae26b36c8944e84
alexjthomson1882 <alexthomson666@gmail.com>
2022-05-07 10:53:58 +0100

    Began adding support for EXTENSIBLE formatted WAVE data importation.

--- Runtime/Media/WavImporter.cs
+++ Runtime/Media/WavImporter.cs
@@ -19,10 +19,11 @@ namespace BlackTundra.ModFramework.Media {
 
         private static readonly ASCIIEncoding Encoding = new ASCIIEncoding();
 
-        private static readonly byte[] RIFFHeaderChunkID = Encoding.GetBytes("RIFF");
-        private static readonly byte[] RIFFHeaderExpectedFileType = Encoding.GetBytes("WAVE");
-        private static readonly byte[] FMT_SubchunkID = Encoding.GetBytes("fmt ");
-        private static readonly byte[] DATASubchunkID = Encoding.GetBytes("data");
+        private static readonly byte[] RIFF_HeaderChunkID = Encoding.GetBytes("RIFF");
+        private static readonly byte[] RIFF_HeaderExpectedFileType = Encoding.GetBytes("WAVE");
+        private static readonly byte[] FMT__SubchunkID = Encoding.GetBytes("fmt ");
+        private static readonly byte[] FACT_SubchunkID = Encoding.GetBytes("fact");
+        private static readonly byte[] DATA_SubchunkID = Encoding.GetBytes("data");
 
         private const float _8BitPerSampleCoefficient = 1.0f / 128.0f;
         private const float _16BitPerSampleCoefficient = 1.0f / 32768.0f;
@@ -31,12 +32,32 @@ namespace BlackTundra.ModFramework.Media {
         #endregion
 
         #region enum
-
+        
+        /// <summary>
+        /// Describes the format of some WAVE data.
+        /// </summary>
         private enum WaveFormat : ushort {
+            /// <summary>
+            /// Short for `Pulse Code Modulation`, PCM is a method to capture waveforms and store the audio digitally. This
+            /// format simply means PCM data is the payload in the WAV data.
+            /// </summary>
             PCM = 0x0001,
+
+            /// <summary>
+            /// Short for `Institute of Electrical and Electronic Engineers Floating point number`, IEEE_FLOAT simply means
+            /// instead of storing raw PCM data as integers, it is instead stored as either 32bit or 64bit floating point
+            /// numbers.
+            /// </summary>
             IEEE_FLOAT = 0x0003,
-            ALAW = 0x0006,
-            MULAW = 0x0007,
+
+            ALAW = 0x0006, MULAW = 0x0007, // UNSUPPORTED
+
+            /// <summary>
+            /// This is a format that indicates there is an extension to the FMT_ sub-chunk. The extension has one field
+            /// (BitsPerSample) which declares the number of "valid" bits per sample. Another field (ChannelMask) contains
+            /// bits which indicate the mapping from channels to loudspeaker positions. The last field (SubFormat) is a
+            /// 16-byte globally unique identifier (GUID).
+            /// </summary>
             EXTENSIBLE = 0xFFFE
         }
 
@@ -48,13 +69,27 @@ namespace BlackTundra.ModFramework.Media {
             if (name == null) throw new ArgumentNullException(nameof(name));
             if (fsr == null) throw new ArgumentNullException(nameof(fsr));
             if (!fsr.IsFile) throw new ArgumentException($"{nameof(fsr)} must reference a file.");
+
             // read wav data:
             if (!FileSystem.Read(fsr, out byte[] bytes, FileFormat.Standard)) {
                 throw new IOException($"Failed to read wav file at `{fsr}`.");
             }
 
-            // WAVE Format Documentation & Specification:
+            /*
+             * TODO:
+             * - Add dynamic sub-chunk ordering (allow the sub-chunks to appear in any order)
+             * - Add support for ignoring unrecognized chunks (such as `JUNK`) and ignoring and moving onto the
+             *   next chunk.
+             * - Review & tidy up audio data processing switch statements. There is a chance I'm converting the
+             *   integers into floats wrong since I didn't research how the conversion should be done.
+             * - Finish implementing the EXTENSIBLE format by adding it to the final switch statement where the
+             *   audio payload is processed.
+             */
+
+            // WAVE Format Documentation & Specifications:
             // http://soundfile.sapp.org/doc/WaveFormat/
+            // http://www-mmsp.ece.mcgill.ca/Documents/AudioFormats/WAVE/WAVE.html
+            // https://tech.ebu.ch/docs/tech/tech3306v1_0.pdf
 
             /*
              * -------------------------------------------------- RIFF HEADER --------------------------------------------------
@@ -64,14 +99,14 @@ namespace BlackTundra.ModFramework.Media {
              */
 
             // ChunkID:
-            if (!bytes.ContentEquals(0, RIFFHeaderChunkID, 0, 4)) throw new FormatException("Invalid RIFF header chunk ID.");
+            if (!bytes.ContentEquals(0, RIFF_HeaderChunkID, 0, 4)) throw new FormatException("Invalid RIFF header chunk ID.");
 
             // ChunkSize:
             uint chunkSize = BinaryPrimitives.ReadUInt32LittleEndian(bytes[4..8]);
             // The ChunkSize is verified later using the equation in the above ChunkSize definition
 
             // Format:
-            if (!bytes.ContentEquals(8, RIFFHeaderExpectedFileType, 0, 4)) throw new FormatException("Invalid RIFF header format field.");
+            if (!bytes.ContentEquals(8, RIFF_HeaderExpectedFileType, 0, 4)) throw new FormatException("Invalid RIFF header format field.");
 
             /*
              * ------------------------------------------------- FMT_ SUBCHUNK -------------------------------------------------
@@ -90,7 +125,7 @@ namespace BlackTundra.ModFramework.Media {
              */
 
             // Subchunk1ID:
-            if (!bytes.ContentEquals(12, FMT_SubchunkID, 0, 4)) throw new FormatException("Invalid FMT_ subchunk ChunkID.");
+            if (!bytes.ContentEquals(12, FMT__SubchunkID, 0, 4)) throw new FormatException("Invalid FMT_ subchunk ChunkID.");
 
             // Subchunk1Size:
             uint subchunk1Size = BinaryPrimitives.ReadUInt32LittleEndian(bytes[16..20]);
@@ -101,6 +136,7 @@ namespace BlackTundra.ModFramework.Media {
                 throw new NotSupportedException($"Unsupported FMT_ subchunk WAVE format `{audioFormatCode}`.");
             }
             WaveFormat audioFormat = (WaveFormat)audioFormatCode;
+            if (audioFormat == WaveFormat.ALAW || audioFormat == WaveFormat.MULAW) throw new NotSupportedException($"WAVE format `{audioFormat}` not supported.");
 
             // NumChannels:
             ushort numChannels = BinaryPrimitives.ReadUInt16LittleEndian(bytes[22..24]);
@@ -134,12 +170,51 @@ namespace BlackTundra.ModFramework.Media {
              * difference to calculate the number of additional bytes of data at the end of this sub-chunk that can
              * be discarded as additional data.
              */
-            int subchunk1AdditionalBytes = (int)(subchunk1Size - 16);
+            int chunkOffset = (int)(subchunk1Size - 16);
+
+            // Create NumSamples (this is defined in the FACT sub-chunk but can otherwise be calculated later):
+            int numSamples = -1;
+
+            // FACT sub-chunk:
+            if (audioFormat == WaveFormat.EXTENSIBLE) {
+                /*
+                 * ------------------------------------------------- FACT SUBCHUNK -------------------------------------------------
+                 * All (compressed) non-PCM formats must have a FACT chunk. The chunk contains at least one value: the number of
+                 * samples in the file.
+                 * Let X = the additional offset from previous chunks:
+                 * 36+X 4   Subchunk3ID     Contains the letters "fact" in big endian ASCII format
+                 * 40+X 4   Subchunk3Size   Number of bytes of data in this chunk.
+                 * 44+X 4   NumSamples      Number of samples (per channel)
+                 * 48+X *                   Misc data.
+                 */
+
+                /*
+                 * We should now check for the presence of the FACT sub-chunk. If the ChunkID is not present we should not throw a
+                 * FormatException; instead, we should just move on and assume the FACT sub-chunk is not included in the file. In the
+                 * case that the FACT sub-chunk is not included, the next expected chunk is the DATA chunk.
+                 */
+                if (bytes.ContentEquals(36 + chunkOffset, FACT_SubchunkID, 0, 4)) { // Subchunk3ID:
+                    // Subchunk3Size:
+                    int subchunk3Size = (int)BinaryPrimitives.ReadUInt32LittleEndian(bytes[(44 + chunkOffset)..(48 + chunkOffset)]);
+
+                    // NumSamples:
+                    numSamples = (int)BinaryPrimitives.ReadUInt32LittleEndian(bytes[(44 + chunkOffset)..(48 + chunkOffset)]);
+
+                    /*
+                     * Here we can calculate the amount of additional data in this subchunk that can be ignored and add it onto the
+                     * chunk offset:
+                     * 
+                     * Since we know the expected size of the current chunk (4) and the actual size, we can find the number of extra
+                     * bytes and add this to the chunk offset:
+                     */
+                    chunkOffset += subchunk3Size - 4;
+                }
+            }
 
             /*
              * ------------------------------------------------- DATA SUBCHUNK -------------------------------------------------
              * This sub-chunk contains the size of the data and the actual audio:
-             * Let X = the number of additional bytes found in the FMT_ subchunk. This can be thought of as an offset.
+             * Let X = the additional offset from previous chunks:
              * 36+X 4   Subchunk2ID     Contains the letters "data" in big endian ASCII format
              * 40+X 4   Subchunk2Size   == NumSamples * NumChannels * BitsPerSample/8
              *                          This is the number of bytes in the data. You can also think of this as the size of the
@@ -148,36 +223,34 @@ namespace BlackTundra.ModFramework.Media {
              */
 
             /*
-             * Here we can calculate the start index for the samples of actual audio based off of the offset from the FMT_ sub-chunk:
+             * Here we can calculate the start index for the samples of actual audio based off of the offset from the previous chunk(s):
              */
-            int sampleStartIndex = 44 + subchunk1AdditionalBytes;
+            int sampleStartIndex = 44 + chunkOffset;
 
             // Subchunk2ID:
-            if (!bytes.ContentEquals(36 + subchunk1AdditionalBytes, DATASubchunkID, 0, 4)) throw new FormatException("Invalid DATA subchunk ChunkID.");
+            if (!bytes.ContentEquals(36 + chunkOffset, DATA_SubchunkID, 0, 4)) throw new FormatException("Invalid DATA subchunk ChunkID.");
 
             // Subchunk2Size:
-            uint subchunk2Size = BinaryPrimitives.ReadUInt32LittleEndian(bytes[(40 + subchunk1AdditionalBytes)..sampleStartIndex]);
+            uint subchunk2Size = BinaryPrimitives.ReadUInt32LittleEndian(bytes[(40 + chunkOffset)..sampleStartIndex]);
 
             /*
-             * DO NOT VERIFY CHUNK SIZE!
-             * There appears to be some kind of padding or rubbish at the end of the file causing this to fail with 90% of files.
-             * 
-            
-            // Verify ChunkSize:
-            uint expectedChunkSize = 4 + (8 + subchunk1Size) + (8 + subchunk2Size);
-            Debug.Log($"{fsr.FileName} {(chunkSize - expectedChunkSize)} {audioFormat} 1:{subchunk1Size} 2:{subchunk2Size} additional space: {subchunk1AdditionalBytes}");
-            if (chunkSize != expectedChunkSize) throw new FormatException($"Unexpected RIFF header ChunkSize (actual: `{chunkSize}`, expected: `{expectedChunkSize}`).");
-            
+             * Here we can verify the ChunkSize. We cannot calculate the exact value for certain since there may be additional data or padding after
+             * the audio data; however, this does mean we can at least calculate a minimum value:
              */
+            uint minimumChunkSize = 4 + (8 + subchunk1Size) + (8 + subchunk2Size);
+            //Debug.Log($"{fsr.FileName} {(chunkSize - minimumChunkSize)} {audioFormat} 1:{subchunk1Size} 2:{subchunk2Size} additional space: {chunkOffset}");
+            if (chunkSize < minimumChunkSize) throw new FormatException($"Unexpected RIFF header ChunkSize (actual: `{chunkSize}`, expected_minimum: `{minimumChunkSize}`).");
 
-            /*
-             * Here we can calculate the number of samples from the Subchunk2Size:
-             * Since the Subchunk2Size is equal to `NumSamples * NumChannels * BitsPerSample/8` we can reverse engineer this to
-             * find NumSamples:
-             * 
-             * Subchunk2Size / (NumChannels * BitsPerSample/8) == NumSamples
-             */
-            int numSamples = (int)(subchunk2Size / blockAlign);
+            if (numSamples == -1) { // the number of samples still has not been calculated
+                /*
+                 * Here we can calculate the number of samples from the Subchunk2Size:
+                 * Since the Subchunk2Size is equal to `NumSamples * NumChannels * BitsPerSample/8` we can reverse engineer this to
+                 * find NumSamples:
+                 * 
+                 * Subchunk2Size / (NumChannels * BitsPerSample/8) == NumSamples
+                 */
+                numSamples = (int)(subchunk2Size / blockAlign);
+            }
 
             /*
              * ----------------------------------------------- AudioClip Samples -----------------------------------------------
@@ -289,7 +362,7 @@ namespace BlackTundra.ModFramework.Media {
                     }
                     break;
                 }
-                default: throw new NotSupportedException($"Wave format `{audioFormat}` not supported.");
+                default: throw new NotSupportedException($"WAVE format `{audioFormat}` not supported.");
             }
 
             // create audio clip:
