eca715dc9857c9a92622ee8e5c27b806bcf7ea1f
alexjthomson1882 <alexthomson666@gmail.com>
2022-05-07 03:37:28 +0100

    Created JsonTokenUtility class to clean up ModPrefab class code

--- Runtime/Prefabs/ModPrefab.cs
+++ Runtime/Prefabs/ModPrefab.cs
@@ -1,5 +1,6 @@
 using BlackTundra.Foundation.Utility;
 using BlackTundra.ModFramework.Importers;
+using BlackTundra.ModFramework.Utility;
 
 using Newtonsoft.Json.Linq;
 
@@ -27,105 +28,6 @@ namespace BlackTundra.ModFramework.Prefabs {
         /// Compiled <see cref="Regex"/> matcher to validate <see cref="ModPrefab"/> names.
         /// </summary>
         public static readonly Regex NameRegex = new Regex(NamePattern, RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline);
-
-        /// <summary>
-        /// Dictionary that maps types to anonymous functions that parse a <see cref="JToken"/> to the specifed type.
-        /// </summary>
-        private static readonly Dictionary<Type, Func<JToken, Type, object>> ValueParserDictionary = new Dictionary<Type, Func<JToken, Type, object>>() {
-
-            #region string
-            { typeof(string), (json, _) => (string)json },
-            #endregion
-
-            #region int
-            { typeof(int), (json, _) => (int)json },
-            #endregion
-
-            #region float
-            { typeof(float), (json, _) => (float)json },
-            #endregion
-
-            #region bool
-            { typeof(bool), (json, _) => (bool)json },
-            #endregion
-
-            #region Vector2
-            { typeof(Vector2), (json, _) => {
-                JArray jsonArray = (JArray)json;
-                if (jsonArray.Count != 2) throw new FormatException($"Invalid {nameof(Vector2)}.");
-                return new Vector2(
-                    (float)jsonArray[0],
-                    (float)jsonArray[1]
-                );
-            }},
-            #endregion
-
-            #region Vector3
-            { typeof(Vector3), (json, _) => {
-                JArray jsonArray = (JArray)json;
-                if (jsonArray.Count != 3) throw new FormatException($"Invalid {nameof(Vector3)}.");
-                return new Vector3(
-                    (float)jsonArray[0],
-                    (float)jsonArray[1],
-                    (float)jsonArray[2]
-                );
-            }},
-            #endregion
-
-            #region Vector4
-            { typeof(Vector4), (json, _) => {
-                JArray jsonArray = (JArray)json;
-                if (jsonArray.Count != 4) throw new FormatException($"Invalid {nameof(Vector4)}.");
-                return new Vector4(
-                    (float)jsonArray[0],
-                    (float)jsonArray[1],
-                    (float)jsonArray[2],
-                    (float)jsonArray[3]
-                );
-            }},
-            #endregion
-
-            #region Quaternion
-            { typeof(Quaternion), (json, _) => {
-                JArray jsonArray = (JArray)json;
-                return jsonArray.Count switch {
-                    3 => Quaternion.Euler(
-                        (float)jsonArray[0],
-                        (float)jsonArray[1],
-                        (float)jsonArray[2]
-                    ),
-                    4 => new Quaternion(
-                        (float)jsonArray[0],
-                        (float)jsonArray[1],
-                        (float)jsonArray[2],
-                        (float)jsonArray[3]
-                    ),
-                    _ => throw new FormatException($"Invalid {nameof(Quaternion)}.")
-                };
-            }},
-            #endregion
-
-            #region Enum
-            { typeof(Enum), (json, targetType) => Enum.Parse(targetType, (string)json)},
-            #endregion
-
-            #region Mesh
-            { typeof(Mesh), (json, _) => Mod.GetAssetFromPath((string)json).value},
-            #endregion
-
-            #region Material
-            { typeof(Material), (json, _) => {
-                throw new NotImplementedException();
-            }},
-            #endregion
-
-            #region PhysicMaterial
-            { typeof(PhysicMaterial), (json, _) => {
-                throw new NotImplementedException();
-            }},
-            #endregion
-
-        };
 
         #endregion
 
@@ -300,12 +202,9 @@ namespace BlackTundra.ModFramework.Prefabs {
                             // get property type:
                             Type propertyType = propertyInfo.PropertyType;
                             // parse json property value and set component property value:
-                            if (ValueParserDictionary.TryGetValue(propertyType, out Func<JToken, Type, object> parser)) {
+                            if (JsonTokenUtility.TryParseToType(property.Value, propertyType, out object value)) {
                                 try {
-                                    propertyInfo.SetValue(
-                                        component,
-                                        parser.Invoke(property.Value, propertyType) // parse property value
-                                    );
+                                    propertyInfo.SetValue(component, value);
                                 } catch (Exception exception) {
                                     PrefabImporter.ConsoleFormatter.Error(
                                         $"Failed to write to property `{propertyName}` on component `{targetType}` on object `{name}` on prefab `{this.name}`.",
